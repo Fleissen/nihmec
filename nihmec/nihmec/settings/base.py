ec08a2258c8b536f26116b411f5e1bbf9ca7e70d
Lekia Prosper <63610232+LekiaAnonim@users.noreply.github.com>
2023-04-22 03:42:33 +0100

    added new media_url for boto3

--- nihmec/nihmec/settings/base.py
+++ nihmec/nihmec/settings/base.py
@@ -111,6 +111,8 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 CSRF_TRUSTED_ORIGINS = [
     'https://delivery-track-production.up.railway.app', 'https://bentchmarck.com']
